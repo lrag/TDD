106badd07f7fb41ac5472c1ade3be23c73b966bd
@lralvarez <you@example.com>
2024-12-04 22:20:55 +0100

    wip

--- Ej03_DBUnit/target/test-classes/db/datos.sql
+++
@@ -1,6 +0,0 @@
-
-insert into pelicula (titulo, director, genero, year) values ('T1','D1','G1',1);
-insert into pelicula (titulo, director, genero, year) values ('T2','D2','G2',2);
-insert into pelicula (titulo, director, genero, year) values ('T3','D3','G3',3);
-insert into pelicula (titulo, director, genero, year) values ('T4','D4','G4',4);
-insert into pelicula (titulo, director, genero, year) values ('T5','D5','G5',5);
